052cd8c25a92934d9fbb79867d44b6431bb17f72
Shaharyar Khan Niazi <sharee.khan03@gmail.com>
2019-11-11 01:33:22 +0500

    made changes to it

--- hello.c
+++ hello.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+
 typedef struct{
 	char name[50];
 	int age;
@@ -10,11 +12,19 @@ int main()
 	int age, height;
 	printf("Type your name\n");
 	scanf("%19s",name);
+	printf("name is %s",name);
+	fflush(stdin);
 	printf("Type your age\n");
 	scanf("%d", &age);
+	printf("%d",age);
 	printf("Type your height\n");
 	scanf("%d" , &height);
-	person someguy = {*name, age, height};
-	printf("the persons name is %s while his age is %d and his height is %d", someguy.name,someguy.age,someguy.height);
+	printf("%d", height);
+	person someguy;
+	strcpy(someguy.name,name);
+	someguy.age = age;
+	someguy.height = height;
+	printf("the persons name is %s", name);
+	printf(" while his age is %d and his height is %d \n",someguy.age, someguy.height);
 	return 0;
 }
